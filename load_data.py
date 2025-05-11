43c52f89c0fc9abc916dbe17d33d9f9cab9fab48
dhamil-bytes <diane64hamilton@gmail.com>
2025-05-10 21:19:31 -0400

    preprocess for nn

--- load_data.py
+++ load_data.py
@@ -14,6 +14,8 @@ from io import BytesIO
 from PIL import Image, ImageDraw
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 
 
 def preview_data():
@@ -90,3 +92,68 @@ def csv_data(data):
         print(ex)
 
     return df
+
+# receives a dataframe and standardizes all float64 data with standardscalar
+# also turns eventDate into datetime object
+def standardize_data(data: pd.DataFrame):
+    """
+    data
+        a pandas dataframe
+    """
+    # transform to datetime
+    if 'eventDate' in data.columns:
+        data['eventDate'] = data['eventDate'].map(lambda x: datetime.fromisoformat(x))
+    
+    # drop redundant data
+    if 'year' and 'month' and 'day' in data.columns:
+        data = data.drop(columns=['year', 'month', 'day'],  axis=1)
+    if 'scientificName' in data.columns:
+        data = data.drop(columns=['scientificName'], axis=1)
+    if 'species' in data.columns:
+        data = data.drop(columns=['species'], axis=1)
+    if 'observationID' in data.columns:
+        data = data.drop(columns=['observationID'], axis=1)
+
+    # get list and zip after dropping necessary cols
+    cols =  data.columns
+    dtypes = data.dtypes
+    coltypes = zip(cols, dtypes)
+
+    # get int64 and binarize that ho
+    norml_targets = ['coorUncert','elevation','landcover']
+    embedding_targets = ['order','family','genus','specificEpithet','region','district']
+    onehot_targets = ['biogeographicalRegion', 'metaSubstrate', 'substrate', 'iucnRedListCategory', 'class', 'phylum', 'kingdom', 'countryCode', 'habitat','poisonous','hasCoordinate']
+    scaler = StandardScaler()
+    encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+    le = LabelEncoder()
+    for c,d in coltypes:
+        # check for unknown vals
+        has_na = data[c].isna()
+
+        # normalize using standardscalar
+        if c in norml_targets:
+            if has_na.sum() != 0:
+                # picking the median for potentially skewed data
+                data.loc[has_na, c] = data[c].median()
+            data[c] = scaler.fit_transform(X=data[[c]])
+
+        # onehot encode
+        elif c in onehot_targets:
+            if has_na.sum() != 0:
+                # fill as UNK
+                data.loc[has_na, c] = 'UNK'
+            data[c] = data[c].astype('category')
+            
+            encoded = encoder.fit_transform(data[[c]])
+            encoded_df = pd.DataFrame(encoded, columns=encoder.get_feature_names_out([c]))
+            encoded_df.index = data.index
+            data = pd.concat([data.drop(columns=[c]), encoded_df], axis=1)
+        
+        elif c in embedding_targets:
+            if has_na.sum() != 0:
+                # fill as UNK
+                data.loc[has_na, c] = 'UNK'
+            data[c] = data[c].astype('category')
+            data[c] = le.fit_transform(data[c])
+
+    return data
